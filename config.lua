8e1ceacde8c0b80c669a124e356572e518163928
Moby <05.hanch.it@gmail.com>
2023-10-17 17:08:11 +0900

    fix(lsp): graphql ftype

--- config.lua
+++ config.lua
@@ -1,8 +1,10 @@
 vim.wo.relativenumber = true
 
 require("lvim.lsp.manager").setup('graphql', {
-  filetypes = { "graphql", "javascriptreact", "typescriptreact" },
+  filetypes = { "graphql", "typescript", "javascript", "javascriptreact", "typescriptreact" },
 })
+
+lvim.builtin.telescope.theme = "center"
 
 reload "user.nvim-tree"
 reload "user.plugins"
